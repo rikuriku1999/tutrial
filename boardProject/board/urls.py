d9478055f54b28d36504996199c5309fdbde2e29
rikuriku1999 <kalashnikova1120@gmail.com>
2020-01-06 13:19:56 +0900

    boardproject

---
+++ boardProject/board/urls.py
@@ -0,0 +1,15 @@
+
+from django.urls import path
+from .views import signupfunc,loginfunc,listfunc,logoutfunc,detailfunc,goodfunc,readfunc,Create
+
+urlpatterns = [
+    path('signup/',signupfunc,name='signup'),
+    path('login/',loginfunc,name='login'),
+    path('list/',listfunc,name='list'),
+    path('logout/',logoutfunc,name='logout'),
+    path('detail/<int:pk>',detailfunc,name='detail'),
+    path('good/<int:pk>',goodfunc,name='good'),
+    path('read/<int:pk>',readfunc,name='read'),
+    path('create/',Create.as_view(),name='create'),
+    
+]
